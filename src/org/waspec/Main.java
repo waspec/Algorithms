d7db427164e256ad8cc8f64818a01ece3f515656
WASPEC <waspec@outlook.com>
2015-03-22 17:49:24 -0700

    Update

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,37 +1,9 @@
 package org.waspec;
 
-import org.waspec.org.waspec.algorithims.trees.binarytrees.BinaryTree;
-import org.waspec.org.waspec.algorithims.trees.binarytrees.Node;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 public class Main {
 
     public static void main(String[] args) {
 
-    }
-
-    // Get all path of binary tree use Backtracking
-    public static void test001(){
-        Node root = BinaryTree.buildTree();
-        List<List<Node>> result = new ArrayList<List<Node>>();
-        Stack<Node> pathBase = new Stack<Node>();
-        try {
-            BinaryTree.getAllPathsUseBacktracking(result, pathBase, root);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        for (List<Node> path : result) {
-            for (Node node : path) {
-                System.out.print(node.payload);
-                System.out.print("->");
-            }
-
-            System.out.println();
-        }
     }
 }
 
